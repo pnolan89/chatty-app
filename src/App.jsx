bc87b62d4081532b7a63c5d87f32c673ecd3b503
pnolan89 <petenol@gmail.com>
2019-02-12 02:55:22 +0000

    split starter  css and html into React modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
   render() {
     return (
     <div>
     <nav className="navbar">
-    <a href="/" className="navbar-brand">Chatty</a>
+      <a href="/" className="navbar-brand">Chatty</a>
     </nav>
-    <main className="messages">
-      <div className="message">
-        <span className="message-username">Anonymous1</span>
-        <span className="message-content">I won't be impressed with technology until I can download food.</span>
-      </div>
-      <div className="message system">
-        Anonymous1 changed their name to nomnom.
-      </div>
-    </main>
+    <MessageList />
     <ChatBar />
     </div>
     );
--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
 const Message = () => {
-    return (
-    <footer className="chatbar">
-      <input className="chatbar-username" placeholder="Your Name (Optional)" />
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-    </footer>
-    );
+  return (
+    <div className="message">
+      <span className="message-username">Anonymous1</span>
+      <span className="message-content">I won't be impressed with technology until I can download food.</span>
+    </div>
+  );
 }
 
 export default Message;
