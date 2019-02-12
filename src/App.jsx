1ced0119b6450c80c3976d622d72344848678611
pnolan89 <petenol@gmail.com>
2019-02-12 20:57:00 +0000

    convert chatbar to form that submits new message data to the app

--- src/App.jsx
+++ src/App.jsx
@@ -19,20 +19,26 @@ class App extends Component {
           username: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
         }
-      ]
+      ],
+      newMessageKey: 3
     };
+    this.addNewMessage = this.addNewMessage.bind(this);
+  }
+
+  addNewMessage(id, username, content) {
+    const newMessage = {id: id, username: username, content: content};
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({
+      messages: messages,
+      newMessageKey: this.state.newMessageKey + 1
+    });
   }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
     setTimeout(() => {
       console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages});
+      this.addNewMessage(this.state.newMessageKey, "Michelle", "Hello there!");
     }, 3000);
   }
 
@@ -43,7 +49,7 @@ class App extends Component {
       <a href="/" className="navbar-brand">Chatty</a>
     </nav>
     <MessageList messages={this.state.messages}/>
-    <ChatBar currentUser={this.state.currentUser.name}/>
+    <ChatBar currentUser={this.state.currentUser.name} addNewMessage={this.addNewMessage} newMessageKey={this.state.newMessageKey}/>
     </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 
 const ChatBar = (props) => {
-    return (
+  const onSubmit = (event) => {
+    event.preventDefault();
+    let id = props.newMessageKey;
+    let username = event.target.username.value;
+    let content = event.target.content.value;
+    props.addNewMessage(id, username, content);
+  };
+  return (
+  <form onSubmit={onSubmit}>
     <footer className="chatbar">
-      <input className="chatbar-username" defaultValue={props.currentUser} />
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input name="username" className="chatbar-username" defaultValue={props.currentUser} />
+        <input name="content" className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input type="submit" />
     </footer>
-    );
+  </form>
+  );
 }
 
 export default ChatBar;
